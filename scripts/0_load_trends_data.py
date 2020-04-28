78688da737ad9330bfb6c5250123f3a82b0c2422
bnebeker <brett.nebeker@outlook.com>
2020-04-28 12:21:30 -0700

    BUG: large list returns error - broke list into chunks for smaller runs

--- scripts/0_load_trends_data.py
+++ scripts/0_load_trends_data.py
@@ -4,11 +4,13 @@ import pandas as pd
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.max_rows', 500)
 
+# larger lists returning 400 error, breaking kw list into smaller pieces
 kw_list = [
     "unemployment",
     "jobs",
     "economy",
     "gross domestic product",
+    "instability",
     "GDP",
     "wages",
     "wage growth",
@@ -20,12 +22,25 @@ kw_list = [
     "pandemic",
     "coronavirus",
     "covid",
-    "instability"
+    "stocks"
 ]
 
-df = google_trends_data(kw_list=kw_list, end_string='2020-05-01')
+chunk_size = 5
+for i in range(0, len(kw_list), chunk_size):
+    list_subset = kw_list[i:i + chunk_size]
+    print(list_subset)
+    _df = google_trends_data(kw_list=list_subset, end_string='2020-05-01')
 
-df.to_csv(
+    if i == 0:
+        output_df = _df.copy()
+    else:
+        output_df = output_df.merge(
+            _df,
+            how='left',
+            on=['geoname', 'date']
+        )
+
+output_df.to_csv(
     './data/google_trends.csv.tar.bz2',
     compression='bz2',
     index=False
