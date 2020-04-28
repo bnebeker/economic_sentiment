08aa8555eade7c05435223c3fca04f544eec152b
bnebeker <brett.nebeker@outlook.com>
2020-04-28 13:18:28 -0700

    REF: combine full us trends & umichigan data

--- scripts/1_combine_data.py
+++ scripts/1_combine_data.py
@@ -1,5 +1,4 @@
 import pandas as pd
-
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.max_rows', 500)
 
@@ -12,7 +11,33 @@ df_s.loc[:, 'day'] = 1
 df_s.loc[:, 'date'] = pd.to_datetime(df_s[['year', 'month', 'day']])
 
 df_t = pd.read_csv(
-    './/data/google_trends.csv.tar.bz2',
+    './data/google_trends_full_us.csv.tar.bz2',
     compression='bz2'
+)
+
+df_t.loc[:, 'date'] = pd.to_datetime(df_t.loc[:, 'date'])
+
+print("SENTIMENT DATA...")
+print(df_s.shape)
+print(df_s.head())
+
+print("")
+print("TRENDS DATA...")
+print(df_t.shape)
+print(df_t.head())
+
+df_s = df_s.loc[:, ['date', 'bus12_r_all', 'umex_r_all']]
+df_s.columns = ['date', 'target_bus12', 'target_umex']
+
+df = df_t.merge(
+    df_s,
+    how='left',
+    on='date'
+)
+
+df.to_csv(
+    './data/combined_data_full_us.csv.tar.bz2',
+    compression='bz2',
+    index=False
 )
 
