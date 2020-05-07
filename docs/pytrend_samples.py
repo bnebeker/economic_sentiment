038a0b3dbd68d64dc6334aa492033308c8dbe9d9
bnebeker <brett.nebeker@outlook.com>
2020-05-06 18:05:44 -0700

    DOC: add geo state-level sample

--- docs/pytrend_samples.py
+++ docs/pytrend_samples.py
@@ -7,6 +7,8 @@ pytrend.build_payload(kw_list=['economy', 'GDP'])
 df = pytrend.interest_by_region()
 df.head(10)
 
+df = pytrend.interest_over_time()
+
 keywords = pytrend.suggestions(keyword='economy')
 df2 = pd.DataFrame(keywords)
 df2.drop(columns='mid')
@@ -19,3 +21,18 @@ related_queries.values()
 
 related_topic = pytrend.related_topics()
 related_topic.values()
+
+
+
+start_date = '2004-01-01'
+end_date = '2010-01-01'
+timeframe = start_date + ' ' + end_date
+
+kw_list = ['economy', 'gdp']
+
+pytrend.build_payload(kw_list, cat=0, timeframe=timeframe, geo='US-AZ', gprop='')
+
+df = pytrend.interest_over_time()
+df.reset_index(inplace=True)
+df.columns = df.columns.str.replace(' ', '_')
+df.columns = map(str.lower, df.columns)
