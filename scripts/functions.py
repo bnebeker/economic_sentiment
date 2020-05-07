8c047b5e8bc48b0150b0dca0b6d48c69be1b5a12
bnebeker <brett.nebeker@outlook.com>
2020-05-07 12:41:15 -0700

    ENH: add pytrends functions with adjusted time-delta, use this class for daily pulls

--- scripts/functions.py
+++ scripts/functions.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from pytrends.request import TrendReq
 import numpy as np
+from scripts.pytrends_functions import TrendReqDaily
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.max_rows', 500)
 
@@ -56,34 +57,17 @@ def google_trends_historical(kw_list=None, year_end='2020', month_end='05', geo=
     return df
 
 
-def google_trends_daily(kw_list=None, start_string='2020-01-01', end_string=None, resolution='REGION'):
-    pytrend = TrendReq()
-    date_list = pd.date_range(start_string, end_string, freq='1D')
+def google_trends_historical_daily(kw_list=None, year_end='2020', month_end='05', geo="US"):
+    pytrend = TrendReqDaily()
 
-    data_list = []
+    start_date = '2004-01-01'
+    end_date = str(year_end) + '-' + str(month_end) + '-' + '01'
+    timeframe = start_date + ' ' + end_date
 
-    for index, dt in enumerate(date_list):
-        next_dt = index + 1
-        if next_dt < len(date_list):
-            timeframe = dt.strftime('%Y-%m-%d') + ' ' + date_list[next_dt].strftime('%Y-%m-%d')
-            print("DATE:", timeframe)
+    pytrend.build_payload(kw_list, cat=0, timeframe=timeframe, geo=geo, gprop='')
 
-            pytrend.build_payload(kw_list, cat=0, timeframe=timeframe, geo='US', gprop='')
-
-            # Interest by region
-            # DMA ~ MAJOR CITY, REGION == STATE
-            _df = pytrend.interest_by_region(
-                resolution=resolution
-            )  # metro level data ( you can change 'DMA' to 'CITY', 'REGION' )
-            _df.reset_index(inplace=True)
-            _df.loc[:, 'date'] = dt
-            print(_df.head())
-            print("\n\n")
-            data_list.append(_df)
-
-    df = pd.concat(data_list)
-    print(df.shape)
-    ## columns w/ spaces to _
+    df = pytrend.interest_over_time()
+    df.reset_index(inplace=True)
     df.columns = df.columns.str.replace(' ', '_')
     df.columns = map(str.lower, df.columns)
 
