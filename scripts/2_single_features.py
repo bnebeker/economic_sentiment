aad2687d718d41a18bab807022420dd8252cf896
bnebeker <brett.nebeker@outlook.com>
2020-05-08 19:24:54 -0700

    REF: rerun single features, move to single file

--- scripts/2_single_features.py
+++ scripts/2_single_features.py
@@ -20,13 +20,11 @@ df = pd.read_csv(
     compression='bz2'
 )
 
-# target = 'target_bus12'
-target = 'target_umex'
-
 features = df.columns[3:]
 
 output = pd.DataFrame(columns=[
     'feature',
+    'target',
     'cardinality',
     'mean',
     'stddev',
@@ -47,71 +45,75 @@ output = pd.DataFrame(columns=[
     'linear_mape'
 ])
 
-for f in features:
-    print(f)
-    x = df.loc[:, f]
-    x = np.array(x).reshape(-1, 1)
-    y = df.loc[:, target]
+target_list = ['target_bus12', 'target_umex']
 
-    print('FITTING DECISION TREE...')
-    tree_mdl = tree.fit(x, y)
-    tree_preds = tree_mdl.predict(x)
+for target in target_list:
+    for f in features:
+        print(f)
+        x = df.loc[:, f]
+        x = np.array(x).reshape(-1, 1)
+        y = df.loc[:, target]
 
-    tree_r2 = r2_score(y, tree_preds)
-    tree_rmse = mean_squared_error(y, tree_preds)
-    tree_mape = mean_absolute_percentage_error(y, tree_preds)
+        print('FITTING DECISION TREE...')
+        tree_mdl = tree.fit(x, y)
+        tree_preds = tree_mdl.predict(x)
 
-    print('FITTING LINEAR MODEL...')
-    linear_mdl = lr.fit(x, y)
-    linear_preds = linear_mdl.predict(x)
+        tree_r2 = r2_score(y, tree_preds)
+        tree_rmse = mean_squared_error(y, tree_preds)
+        tree_mape = mean_absolute_percentage_error(y, tree_preds)
 
-    linear_r2 = r2_score(y, linear_preds)
-    linear_rmse = mean_squared_error(y, linear_preds)
-    linear_mape = mean_absolute_percentage_error(y, linear_preds)
+        print('FITTING LINEAR MODEL...')
+        linear_mdl = lr.fit(x, y)
+        linear_preds = linear_mdl.predict(x)
 
-    # general feature metrics
-    cardinality = df.loc[:, f].nunique()
-    col_mean, stddev, min, p25, p50, p75, max = df.loc[:, f].describe()[[1, 2, 3, 4, 5, 6, 7]]
-    skew = df.loc[:, f].skew()
-    kurt = df.loc[:, f].kurt()
-    p5 = df.loc[:, f].quantile(0.05)
-    p95 = df.loc[:, f].quantile(0.95)
+        linear_r2 = r2_score(y, linear_preds)
+        linear_rmse = mean_squared_error(y, linear_preds)
+        linear_mape = mean_absolute_percentage_error(y, linear_preds)
 
-    _tmp = pd.DataFrame(columns=[
-        'feature',
-        'cardinality',
-        'mean',
-        'stddev',
-        'min',
-        'p5',
-        'p25',
-        'p50',
-        'p75',
-        'p95',
-        'max',
-        'kurtosis',
-        'skew',
-        'tree_r2',
-        'tree_rmse',
-        'tree_mape',
-        'linear_r2',
-        'linear_rmse',
-        'linear_mape'
-    ])
+        # general feature metrics
+        cardinality = df.loc[:, f].nunique()
+        col_mean, stddev, min, p25, p50, p75, max = df.loc[:, f].describe()[[1, 2, 3, 4, 5, 6, 7]]
+        skew = df.loc[:, f].skew()
+        kurt = df.loc[:, f].kurt()
+        p5 = df.loc[:, f].quantile(0.05)
+        p95 = df.loc[:, f].quantile(0.95)
 
-    _tmp.loc[0] = [
-        f, cardinality, col_mean, stddev, min, p5, p25, p50,
-        p75, p95, max, kurt, skew, tree_r2, tree_rmse, tree_mape,
-        linear_r2, linear_rmse, linear_mape
-    ]
+        _tmp = pd.DataFrame(columns=[
+            'feature',
+            'target',
+            'cardinality',
+            'mean',
+            'stddev',
+            'min',
+            'p5',
+            'p25',
+            'p50',
+            'p75',
+            'p95',
+            'max',
+            'kurtosis',
+            'skew',
+            'tree_r2',
+            'tree_rmse',
+            'tree_mape',
+            'linear_r2',
+            'linear_rmse',
+            'linear_mape'
+        ])
 
-    output = output.append(_tmp, ignore_index=True)
+        _tmp.loc[0] = [
+            f, target, cardinality, col_mean, stddev, min, p5, p25, p50,
+            p75, p95, max, kurt, skew, tree_r2, tree_rmse, tree_mape,
+            linear_r2, linear_rmse, linear_mape
+        ]
+
+        output = output.append(_tmp, ignore_index=True)
 
 output.sort_values(by='linear_r2', ascending=False, inplace=True)
 print(output.head(10))
 
 output.to_csv(
-    './assets/outputs/single_feature_report_{}.csv.tar.bz2'.format(target),
+    './assets/outputs/single_feature_report.csv.tar.bz2'.format(target),
     compression='bz2',
     index=False
 )
